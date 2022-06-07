2b2b36465576eec504bafa16006d149b90a86ef6
kagorax <kelan360@gmail.com>
2022-06-07 09:19:14 +0100

    update dependencies

--- src/main/java/com/tsi/kelan/FirstSpringProject/ActorRepository.java
+++ src/main/java/com/tsi/kelan/FirstSpringProject/ActorRepository.java
@@ -2,5 +2,7 @@ package com.tsi.kelan.FirstSpringProject;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ActorRepository extends CrudRepository<Actor,Integer> {
 }
--- src/main/java/com/tsi/kelan/FirstSpringProject/FirstSpringProjectApplication.java
+++ src/main/java/com/tsi/kelan/FirstSpringProject/FirstSpringProjectApplication.java
@@ -3,10 +3,9 @@ package com.tsi.kelan.FirstSpringProject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @SpringBootApplication
 @RestController
@@ -34,4 +33,10 @@ public class FirstSpringProjectApplication {
 	Iterable<Actor>getAllActors(){
 		return actorRepository.findAll();
 	}
+
+	//@DeleteMapping("Delete")
+	//public void @ResponseBody
+
+
+
 }
