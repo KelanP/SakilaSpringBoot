96adb521929eead0e06d6a834723c12f2a5fea07
kagorax <kelan360@gmail.com>
2022-06-07 14:53:32 +0100

    Working on test file

--- src/test/java/com/tsi/kelan/FirstSpringProject/ActorTest.java
+++ src/test/java/com/tsi/kelan/FirstSpringProject/ActorTest.java
@@ -7,11 +7,13 @@ public class ActorTest {
 
 
     @Test
-    public void testGettersandSetters(){
+    public void testGettersAndSetters(){
         Actor testActor =new Actor();
         testActor.setFirst_name("Bondman");
         testActor.setLast_name("Namdnob");
+        testActor.setActor_id(9340);
         Assertions.assertEquals("Bondman",testActor.getFirst_name());
         Assertions.assertEquals("Namdnob",testActor.getLast_name());
+        Assertions.assertEquals(9340,testActor.getActor_id());
     }
 }
