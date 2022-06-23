34629f25e38c47d93122a1924ce377eb66d5110a
KelanP <kelan360@gmail.com>
2022-06-23 10:27:16 +0100

    FilmActorTest test complete

--- src/test/java/com/tsi/kelan/SakilaSpringProject/entityTests/FilmActorTest.java
+++ src/test/java/com/tsi/kelan/SakilaSpringProject/entityTests/FilmActorTest.java
@@ -4,6 +4,7 @@ package com.tsi.kelan.SakilaSpringProject.entityTests;
 import com.tsi.kelan.SakilaSpringProject.entities.Actor;
 import com.tsi.kelan.SakilaSpringProject.entities.Film;
 import com.tsi.kelan.SakilaSpringProject.entities.FilmActor;
+import com.tsi.kelan.SakilaSpringProject.entities.FilmActorId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,26 +17,43 @@ public class FilmActorTest {
 
         Actor testActor =new Actor(20,"John","Paul");
         Film testFilm =  new Film(2,"My New Solo Film","a trashy film about han solo", 2022,0, "null",3,2.99f,200,22.99f,"R","null","2022-02-15 15:03:42");
+        FilmActorId testFilmActorId = new FilmActorId();
 
         FilmActor testFilmActor =new FilmActor();
 
         testFilmActor.setActor(testActor);
         testFilmActor.setFilm(testFilm);
         testFilmActor.setLastUpdate("2017-12-03 10:15:30");
+        testFilmActor.setId(testFilmActorId);
 
 
 
         Assertions.assertEquals(testActor, testFilmActor.getActor());
         Assertions.assertEquals(testFilm, testFilmActor.getFilm());
         Assertions.assertEquals("2017-12-03 10:15:30", testFilmActor.getLastUpdate());
+        Assertions.assertEquals(testFilmActorId, testFilmActor.getId());
+
 
 
 
 
     }
 
-//    @Test
-//    public void testConstructor(){
-//
+    @Test
+    public void testConstructor(){
+
+        Actor testActor =new Actor();
+        Film testFilm =  new Film();
+        FilmActorId testFilmActorId = new FilmActorId();
+
+        FilmActor testFilmActor = new FilmActor(testFilmActorId,testActor,testFilm,"2018-12-03 10:15:30");
+
+        Assertions.assertEquals(testActor, testFilmActor.getActor());
+        Assertions.assertEquals(testFilm, testFilmActor.getFilm());
+        Assertions.assertEquals("2018-12-03 10:15:30", testFilmActor.getLastUpdate());
+        Assertions.assertEquals(testFilmActorId, testFilmActor.getId());
+
+    }
+
 
 }
